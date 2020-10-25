0338d8b656a7810d8e3d60e34a992a8099d31290
Lorenz Gillner <nospampls@aaathats3as.com>
2020-10-25 13:07:24 +0100

    cleanup

--- manga_update.c
+++ manga_update.c
@@ -1,11 +1,15 @@
-
 #include <stdio.h>
-int main(){
-        system("rclone sync /root/storage/Media/Mango/Manga/ /root/manga/");
-        system("rclone sync /root/storage/Media/Mango/H-Manga/ /root/h/");
-        system("rclone sync /root/storage/Media/Mango/Manwah/ /root/manwah/");
-        system("rclone sync /root/storage/Media/Mango/comic/  /root/comics/");
-        system("rclone sync /root/storage/Media/Mango/H-Books/  /root/h-manga/");
-        system("rclone sync /root/storage/Media/Mango/Manga_offiziell/  /root/manga_offiziell/");
-        return 0;
+#include <stdlib.h>
+
+int
+main(void)
+{
+	system("rclone sync /root/storage/Media/Mango/Manga/ /root/manga/");
+	system("rclone sync /root/storage/Media/Mango/H-Manga/ /root/h/");
+	system("rclone sync /root/storage/Media/Mango/Manwah/ /root/manwah/");
+	system("rclone sync /root/storage/Media/Mango/comic/  /root/comics/");
+	system("rclone sync /root/storage/Media/Mango/H-Books/  /root/h-manga/");
+	system("rclone sync /root/storage/Media/Mango/Manga_offiziell/  /root/manga_offiziell/");
+        
+	return EXIT_SUCCESS;
 }
