17ecdac0ed5ab44cf2af69665a7b33e1633d1525
Lorenz Gillner <nospampls@aaathats3as.com>
2020-10-25 16:24:54 +0100

    look for sync command first

--- manga_update.sh
+++ manga_update.sh
@@ -1,14 +1,26 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-CMD="rclone sync -P"
+CMD="rclone"
+OPT="sync -P"
 BASE_DIR="/root/storage/Media/Mango"
 
-$CMD "$BASE_DIR/Manga/" "/root/manga/"
-$CMD "$BASE_DIR/H-Manga/" "/root/h/"
-$CMD "$BASE_DIR/Manwah/" "/root/manwah/"
-$CMD "$BASE_DIR/comic/" "/root/comics/"
-$CMD "$BASE_DIR/H-Books/" "/root/h-manga/"
-$CMD "$BASE_DIR/Manga_offiziell/" "/root/manga_offiziell/"
+if hash $CMD &>/dev/null
+then
+	printf "$CMD is not installed. Do you want to install it? (y/n)"
+	read -s -n 1 e 
+	printf "\n"
+	case $e in
+		[Yy]) apt install -y $CMD ;;
+		[Nn]) exit 1 ;;
+	esac
+fi
+
+$CMD $OPT "$BASE_DIR/Manga/" "/root/manga/"
+$CMD $OPT "$BASE_DIR/H-Manga/" "/root/h/"
+$CMD $OPT "$BASE_DIR/Manwah/" "/root/manwah/"
+$CMD $OPT "$BASE_DIR/comic/" "/root/comics/"
+$CMD $OPT "$BASE_DIR/H-Books/" "/root/h-manga/"
+$CMD $OPT "$BASE_DIR/Manga_offiziell/" "/root/manga_offiziell/"
 echo "Done!"
 
 exit 0
