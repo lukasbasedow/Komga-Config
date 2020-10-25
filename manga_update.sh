ab345fb2b999f262f03d5624919705040e8f3004
Lorenz Gillner <nospampls@aaathats3as.com>
2020-10-25 15:55:08 +0100

    polished shell script

--- manga_update.sh
+++ manga_update.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
 
-rclone sync /root/storage/Media/Mango/Manga/ /root/manga/
-rclone sync /root/storage/Media/Mango/H-Manga/ /root/h/
-rclone sync /root/storage/Media/Mango/Manwah/ /root/manwah/
-rclone sync /root/storage/Media/Mango/comic/  /root/comics/
-rclone sync /root/storage/Media/Mango/H-Books/  /root/h-manga/
-rclone sync /root/storage/Media/Mango/Manga_offiziell/  /root/manga_offiziell/
+CMD="rclone sync"
+BASE_DIR="/root/storage/Media/Mango"
 
+$CMD "$BASE_DIR/Manga/" "/root/manga/"
+$CMD "$BASE_DIR/H-Manga/" "/root/h/"
+$CMD "$BASE_DIR/Manwah/" "/root/manwah/"
+$CMD "$BASE_DIR/comic/" "/root/comics/"
+$CMD "$BASE_DIR/H-Books/" "/root/h-manga/"
+$CMD "$BASE_DIR/Manga_offiziell/" "/root/manga_offiziell/"
+
+exit 0
